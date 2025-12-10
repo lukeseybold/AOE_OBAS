374090ca40be8523d6c718dc5f500df98cb72f3b
lukeseybold <lukeseybold@gmail.com>
2025-12-10 14:08:07 -0800

    Update model

--- 1device/NEMOH/stabilizer/runnemoh.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start  "C:\Users\wcwi\Desktop\NEMOH\" mesh.exe
-start  "C:\Users\wcwi\Desktop\NEMOH\" preProc.exe 
-start  "C:\Users\wcwi\Desktop\NEMOH\" solver.exe  
-start  "C:\Users\wcwi\Desktop\NEMOH\" postProc.exe  
--- 1device/WECSim/lib/MOST/slblocks.m
+++
@@ -1,8 +0,0 @@
-function blkStruct = slblocks
-% This function specifies that the library 'MOST_Lib'
-% should appear in the Library Browser with the 
-% name 'MOST'
-
-Browser.Library = 'MOST_Lib';
-Browser.Name = 'MOST';
-blkStruct.Browser = Browser;
